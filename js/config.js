aaaa6fa807274809a112d242227564026bb89fa9
Jose Luis <sisjosex@gmail.com>
2014-11-10 13:58:55 -0400

    Updates

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 /************************************ VARIABLES DE CONFIGURACION *******************************************************/
 
 /************************************ server *******************************************************/
-var BASE_URL_APP = "http://queplan.sandbox.com:8080/";
+//var BASE_URL_APP = "http://queplan.sandbox.com/";
 
-//var BASE_URL_APP = "http://www.teappuntas.es/admin/";
+var BASE_URL_APP = "http://www.teappuntas.es/webapps/";
 
 /************************************ localhost *******************************************************/
 //var BASE_URL_APP = "http://localhost/queplan/web/";
